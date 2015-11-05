c71713acc29cb343c840d16b0f2931622d47d2f0
Dan Winter <dan.j.winter@gmail.com>
2015-11-05 07:01:53 -0700

    cleaned up http server with refactoring into smaller methods

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -31,52 +31,71 @@ class Server
       @client = tcp_server.accept
 
       puts "Ready for a request"
-      request_lines = []
 
-      while line = @client.gets and !line.chomp.empty?
-        request_lines << line.chomp
-      end
-
-      @responder = Response.new(request_lines, self)
-
-
-      # if @responder.parsed.path == "/new_game" && @responder.parsed.verb == "POST"
-      #   @new_game = Game.new
-      if responder.parsed.path == "/start_game"&& @responder.parsed.verb == "POST"
-        @new_game = Game.new
-      end
-
-      if @responder.parsed.path == "/game" && @responder.parsed.verb == "POST"
-        @game_counter += 1
-        web_kit_counter = 0
-        @body_params = []
-
-        until web_kit_counter == 2
-          line = @client.gets
-          @body_params << line.chomp
-
-          if line.start_with?("------WebKitFormBoundary")
-            web_kit_counter += 1
-          end
-        end
-      end
-      begin
-      @client.puts @responder.response_compiler(self)
-    rescue => detail
-      response = "<pre>#{detail.backtrace.join("\n")}</pre>".delete!("<main>")
-      output = "<html><head></head><body>#{response}</body></html>"
-      headers = ["http/1.1 500 Error",
-                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-                "server: ruby",
-                "content-type: text/html; charset=iso-8859-1",
-                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-      @client.puts headers
-      @client.puts output
-    end
+      generate_and_send_response
 
       break if @responder.parsed.path == "/shutdown"
 
       @client.close
+    end
+  end
+end
+
+def generate_and_send_response
+  headers_from_request
+
+  start_new_game
+
+  body_parameters
+
+  send_response
+end
+
+def send_response
+  begin
+    @client.puts @responder.response_compiler(self)
+  rescue => detail
+    response = "<pre>#{detail.backtrace.join("\n")}</pre>".delete!("<main>")
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers = ["http/1.1 500 Error",
+      "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+      "server: ruby",
+      "content-type: text/html; charset=iso-8859-1",
+      "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+      @client.puts headers
+      @client.puts output
+    end
+  end
+
+def start_new_game
+  if @responder.parsed.path == "/start_game"&& @responder.parsed.verb == "POST"
+    @new_game = Game.new
+  end
+end
+
+def headers_from_request
+  request_lines = []
+
+  while line = @client.gets and !line.chomp.empty?
+    request_lines << line.chomp
+  end
+
+  @responder = Response.new(request_lines, self)
+end
+
+def body_parameters
+  if @responder.parsed.path == "/game" && @responder.parsed.verb == "POST"
+    @game_counter += 1
+    web_kit_counter = 0
+    @body_params = []
+
+    until web_kit_counter == 2
+      line = @client.gets
+      @body_params << line.chomp
+
+      if line.start_with?("------WebKitFormBoundary")
+        web_kit_counter += 1
+      end
     end
   end
 end
